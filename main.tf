b0195bad7d24894c0668ccdf665d54abfe4a5e39
yaohull <yaohual.liu@gmail.com>
2022-12-14 16:43:48 +0800

    add S3 lifecycle & kuctl dockerfile 2022.12.14

--- main.tf
+++ main.tf
@@ -11,12 +11,51 @@ terraform {
 
 #Resource to create s3 bucket
 resource "aws_s3_bucket" "tf_test_bucket" {
-  bucket = "yaohua-tf-test-bucket"
+    bucket = "yaohua-tf-test-bucket"
+    force_destroy = true
+    tags = {
+        Name = "My bucket"
+        Environment = "Dev"
+  }
 }
 
 resource "aws_s3_bucket_acl" "tf_test_bucket_acl" {
-  bucket = aws_s3_bucket.tf_test_bucket.id
-  acl    = var.acl
+    bucket = aws_s3_bucket.tf_test_bucket.id
+    acl    = var.acl
 }
 
+resource "aws_s3_bucket_versioning" "tf_test_versioning" {
+    bucket = aws_s3_bucket.tf_test_bucket.id
+    versioning_configuration {
+    status = "Enabled"
+    }
+}
 
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+
+    bucket = aws_s3_bucket.tf_test_bucket.bucket
+
+    rule {
+    id = "outdated"
+
+    filter {
+        
+        and {
+            # subset
+            prefix = "/"
+
+            tags = {
+                rule = "outdated"
+                autoclean = "false"
+            }
+        }
+    }
+
+    status = "Enabled"
+
+    transition {
+        days          = 30
+        storage_class = "GLACIER"
+    }
+    }
+}
